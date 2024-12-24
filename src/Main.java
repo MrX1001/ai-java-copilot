94c200b01f00baed2bbe14bb88efb47df09e9da5
MrX1001 <40733245+MrX1001@users.noreply.github.com>
2024-12-24 10:28:13 +0200

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {   
+    //Check12
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println("The result is: " + result);
+    }
+    public static int add(a, b) {
+        return a + b;
+    }
+}
